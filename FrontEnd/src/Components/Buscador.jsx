53b3548bb5ef5661a531afb23aaaa9e064659aa5
RodrigoE <esperfotografia94@gmail.com>
2024-06-30 05:52:30 -0300

    falta terminar el buscador y el filtro al crud se encuentra

---
+++ FrontEnd/src/Components/Buscador.jsx
@@ -0,0 +1,41 @@
+import "../CSS/Mains/Buscador.css";
+import { useState } from "react";
+import axios from "axios";
+
+const Buscador = () => {
+  const [cliente, setCliente] = useState("");
+
+        
+
+ 
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let response = await axios.get(`http://localhost:8000/clientes`);
+    console.log(response.data);
+    
+  };
+
+  return (
+    <div>
+      <nav className=" buscador navbar navbar-light bg-body-tertiary">
+        <form  onSubmit={handleSubmit} className="d-flex  input-group w-50" >
+          <input
+            type="search"
+            onChange={(e) => setCliente(e.target.value)}
+            className="form-control rounded"
+            placeholder="Buscar"
+            aria-label="Search"
+            aria-describedby="search-addon"
+          />
+        <button  type="submit" className="boton btn btn-primary">
+          <i> Buscar📥</i>
+        </button>
+        </form>
+      </nav>
+        
+    </div>
+  );
+};
+
+export default Buscador;
--- FrontEnd/src/Components/Mains/MainCliente.jsx
+++ FrontEnd/src/Components/Mains/MainCliente.jsx
@@ -1,39 +1,55 @@
-import {Table} from 'react-bootstrap'
+import { Table } from "react-bootstrap";
+import Buscador from "../Buscador";
+import axios from "axios";
+import {useEffect, useState } from "react";
+
 const MainCliente = () => {
+  const [clientes, setClientes] = useState([]);
+
+  const showClientes = async () => {
+    let response = await axios.get(`http://localhost:8000/clientes`);
+    setClientes(response.data);
+   
+  };
+
+useEffect( ()=>{
+  showClientes()
+},[])
+
+
   return (
     <div>
       <h1 className="text-center text-white p-5">MODIFICA TUS CLIENTES</h1>
+      <Buscador />
       <Table striped bordered hover variant="dark">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </Table>
-    </div>
-  )
-}
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nombre Completo</th>
+            <th>DNI</th>
+            <th>Telefono</th>
+            <th>Direccion</th>
+            <th>Activo</th>
+          </tr>
+        </thead>
+        <tbody>
+          {clientes.map(cliente => 
+            <tr key={cliente.id_cliente}>
+              <td>{cliente.id_cliente}</td>
+              <td>{cliente.nombreCompleto}</td>
+              <td>{cliente.telefono}</td>
+              <td>{cliente.direccion}</td>
+              <td>{cliente.activo}</td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
 
-export default MainCliente
+
+
+    
+    </div>
+  );
+};
+
+export default MainCliente;
